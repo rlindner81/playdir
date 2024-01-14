544c803514ab52d535ff38d6f50892ba1379f63c
Richard Lindner <rlindner81@gmail.com>
2024-01-14 12:26:08 +0100

    more baby steps

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use plist::Value;
-use std::collections::HashMap;
+use std::collections::{BTreeMap, HashMap};
 use std::error::Error;
 use std::fs::File;
 use std::io::{BufReader, BufWriter, Write};
@@ -8,9 +8,9 @@ use std::{env, fs, process};
 
 const STATE_JSON_FILE: &str = "state.json";
 
-fn read_video_times_from_file<P: AsRef<Path>>(
-    path: P,
-) -> Result<HashMap<String, f64>, Box<dyn Error>> {
+type VideoTimes = BTreeMap<String, f64>;
+
+fn read_video_times_from_file<P: AsRef<Path>>(path: P) -> Result<VideoTimes, Box<dyn Error>> {
     let file = File::open(path)?;
     let reader = BufReader::new(file);
     let result = serde_json::from_reader(reader)?;
@@ -19,7 +19,7 @@ fn read_video_times_from_file<P: AsRef<Path>>(
 
 fn write_video_times_to_file<P: AsRef<Path>>(
     path: P,
-    data: HashMap<String, f64>,
+    data: VideoTimes,
 ) -> Result<(), Box<dyn Error>> {
     let file = File::create(path)?;
     let mut writer = BufWriter::new(file);
@@ -35,18 +35,28 @@ fn process_args(args: &[String]) -> Option<&String> {
     None
 }
 
-fn fill_mkv_files(output: &mut Vec<String>, dir_path: &String) {
-    let entries = fs::read_dir(dir_path).unwrap();
+fn fill_video_times_from_dir(
+    video_times: &mut VideoTimes,
+    dir_path: &String,
+) -> Result<(), Box<dyn Error>> {
+    let mut video_files: Vec<String> = Vec::new();
+    let entries = fs::read_dir(dir_path)?;
     for entry in entries {
-        let filepath = entry.unwrap().path();
-        if filepath.extension().unwrap() == "mkv" {
-            output.push(filepath.to_string_lossy().parse().unwrap());
+        let filepath = entry?.path();
+        if filepath.extension() == Some("mkv".as_ref()) {
+            video_files.push(filepath.to_string_lossy().parse()?);
         }
     }
-    output.sort()
+    video_files.sort();
+    for video_file in video_files {
+        if !video_times.contains_key(&video_file) {
+            video_times.insert(video_file, 0.0);
+        }
+    }
+    Ok(())
 }
 
-fn fill_recent_played_media(output: &mut HashMap<String, f64>) {
+fn fill_recent_played_media(output: &mut VideoTimes) {
     let env_home = env::var("HOME").unwrap();
     let vlc_preferences_path = format!("{}/Library/Preferences/org.videolan.vlc.plist", env_home);
     let vlc_preferences = Value::from_file(vlc_preferences_path).unwrap();
@@ -63,7 +73,7 @@ fn fill_recent_played_media(output: &mut HashMap<String, f64>) {
     }
 }
 
-fn fill_mkv_files_playtime(output: &mut HashMap<String, f64>, mkv_files: Vec<String>) {
+fn fill_mkv_files_playtime(output: &mut VideoTimes, mkv_files: Vec<String>) {
     for mkv_file in mkv_files {
         let container = matroska::open(&mkv_file).unwrap();
         let duration = container.info.duration.unwrap().as_secs();
@@ -84,18 +94,21 @@ fn main() {
 
     let mut video_times = match read_video_times_from_file(STATE_JSON_FILE) {
         Ok(result) => result,
-        _ => HashMap::new(),
+        _ => BTreeMap::new(),
     };
 
-    let mut mkv_files: Vec<String> = Vec::new();
-    fill_mkv_files(&mut mkv_files, dir_path);
+    fill_video_times_from_dir(&mut video_times, dir_path).unwrap();
 
-    let mut mkv_files_playtime: HashMap<String, f64> = HashMap::new();
-    fill_mkv_files_playtime(&mut mkv_files_playtime, mkv_files);
+    for (key, value) in &video_times {
+        println!("{:?} has {:?}", key, value);
+    }
 
-    write_video_times_to_file(STATE_JSON_FILE, mkv_files_playtime).unwrap();
-    let mut recent_played_media: HashMap<String, f64> = HashMap::new();
-    fill_recent_played_media(&mut recent_played_media);
+    // let mut mkv_files_playtime: VideoTimes = BTreeMap::new();
+    // fill_mkv_files_playtime(&mut mkv_files_playtime, mkv_files);
+
+    // write_video_times_to_file(STATE_JSON_FILE, mkv_files_playtime).unwrap();
+    // let mut recent_played_media: VideoTimes = BTreeMap::new();
+    // fill_recent_played_media(&mut recent_played_media);
 
     println!("break here");
 }
