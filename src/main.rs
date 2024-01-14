96c947fc9d3012e53ae0de8209de0e2ef0298540
Richard Lindner <rlindner81@gmail.com>
2024-01-14 11:55:05 +0100

    slow start 11

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,30 @@
-use std::{env, fs, process};
+use plist::Value;
 use std::collections::HashMap;
-use plist::{Value};
+use std::error::Error;
+use std::fs::File;
+use std::io::{BufReader, BufWriter, Write};
+use std::path::Path;
+use std::{env, fs, process};
+
+const STATE_JSON_FILE: &str = "state.json";
+
+fn read_video_times_from_file<P: AsRef<Path>>(path: P) -> Result<HashMap<String, f64>, Box<dyn Error>> {
+  let file = File::open(path)?;
+  let reader = BufReader::new(file);
+  let result = serde_json::from_reader(reader)?;
+  Ok(result)
+}
+
+fn write_video_times_to_file<P: AsRef<Path>>(
+  path: P,
+  data: HashMap<String, f64>,
+) -> Result<(), Box<dyn Error>> {
+  let file = File::create(path)?;
+  let mut writer = BufWriter::new(file);
+  serde_json::to_writer(&mut writer, &data)?;
+  writer.flush()?;
+  Ok(())
+}
 
 fn process_args(args: &[String]) -> Option<&String> {
   if args.len() == 1 {
@@ -25,9 +49,12 @@ fn fill_recent_played_media(output: &mut HashMap<String, f64>) {
   let vlc_preferences_path = format!("{}/Library/Preferences/org.videolan.vlc.plist", env_home);
   let vlc_preferences = Value::from_file(vlc_preferences_path).unwrap();
   let recent_played_media_dict = vlc_preferences
-    .as_dictionary().unwrap()
-    .get("recentlyPlayedMedia").unwrap()
-    .as_dictionary().unwrap();
+    .as_dictionary()
+    .unwrap()
+    .get("recentlyPlayedMedia")
+    .unwrap()
+    .as_dictionary()
+    .unwrap();
   for (key, value) in recent_played_media_dict {
     let filepath = key.trim_start_matches("file://").parse().unwrap();
     output.insert(filepath, value.as_signed_integer().unwrap() as f64);
@@ -53,15 +80,21 @@ fn main() {
     }
   };
 
+  let video_times = match read_video_times_from_file(STATE_JSON_FILE) {
+    Ok(result) => result,
+    _ => HashMap::new()
+  };
+
+
   let mut mkv_files: Vec<String> = Vec::new();
   fill_mkv_files(&mut mkv_files, dir_path);
 
   let mut mkv_files_playtime: HashMap<String, f64> = HashMap::new();
   fill_mkv_files_playtime(&mut mkv_files_playtime, mkv_files);
 
+  write_video_times_to_file("test.json", mkv_files_playtime).unwrap();
   let mut recent_played_media: HashMap<String, f64> = HashMap::new();
   fill_recent_played_media(&mut recent_played_media);
 
   println!("break here");
-
 }
