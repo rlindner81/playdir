757109dd181db7795b3e1742afc6b245c76012af
Richard Lindner <rlindner81@gmail.com>
2024-01-14 10:09:35 +0100

    slow start 5

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,8 @@
 use std::{env, fs, process, io};
+use std::collections::HashMap;
+use std::iter::Map;
+use plist::{Error, Value};
+use plist::Value::Integer;
 
 fn process_args(args: &[String]) -> Option<&String> {
   if args.len() == 1 {
@@ -14,10 +18,19 @@ fn process_dir(dir_path: &String) -> Result<fs::ReadDir, io::Error> {
   }
 }
 
-// fn read_plist_entries() -> Result<(), ()> {
-//   println!("lala");
-//   Ok(())
-// }
+fn fill_recent_played_media(output: &mut HashMap<String, i32>) {
+  let env_home = env::var("HOME").unwrap();
+  let vlc_preferences_path = format!("{}/Library/Preferences/org.videolan.vlc.plist", env_home);
+  let vlc_preferences = Value::from_file(vlc_preferences_path).unwrap();
+  let recent_played_media_dict = vlc_preferences
+    .as_dictionary().unwrap()
+    .get("recentlyPlayedMedia").unwrap()
+    .as_dictionary().unwrap();
+  for (key, value) in recent_played_media_dict {
+    let filepath = key.trim_start_matches("file://").parse().unwrap();
+    output.insert(filepath, value.as_signed_integer().unwrap() as i32);
+  }
+}
 
 fn main() {
   let args: Vec<String> = env::args().collect();
@@ -30,21 +43,18 @@ fn main() {
     }
   };
 
-  let entries = match process_dir(dir_path) {
-    Ok(entries) => entries,
-    Err(err) => {
-      eprintln!("Error reading dir '{}': {}", dir_path, err);
-      process::exit(1);
-    }
-  };
+  // let mut
+  // let entries = match process_dir(dir_path) {
+  //   Ok(entries) => entries,
+  //   Err(err) => {
+  //     eprintln!("Error reading dir '{}': {}", dir_path, err);
+  //     process::exit(1);
+  //   }
+  // };
 
-  for entry in entries {
-    match entry {
-      Ok(entry) => {
-        println!("{:?}", entry.path());
-      }
-      Err(_) => ()
-    }
-  }
+  let mut recent_played_media: HashMap<String, i32> = HashMap::new();
+  fill_recent_played_media(&mut recent_played_media);
+  println!("recent_played_media: {:?}", recent_played_media);
 
 }
+
